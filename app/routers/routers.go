eafbf3982cca02813831c55f3cbf09164aa5729c
Hashfyre <joy.bhattacherjee@gmail.com>
2020-05-19 13:14:28 +0530

    registers routes + middlewares

---
+++ app/routers/routers.go
@@ -0,0 +1,57 @@
+package routers
+
+import (
+	"net/http"
+
+	swaggerFiles "github.com/swaggo/files" // swaggo embed files
+	ginSwagger "github.com/swaggo/gin-swagger"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/hashfyre/sample-go-app/app/handlers"
+	"github.com/hashfyre/sample-go-app/app/middlewares"
+	"github.com/hashfyre/sample-go-app/app/types"
+	_ "github.com/hashfyre/sample-go-app/docs" // Swaggo dependency
+)
+
+var router *gin.Engine
+
+// Setup - sets up gin routes with specific handlers
+// chains middlewares
+func Setup() *gin.Engine {
+	router := gin.New()
+	router.GET("/healthz/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "sample-go-app live"})
+	})
+
+	// Setting up middlewares
+
+	// Defaults - need to make this optional only on test mode
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+
+	// Context Metadata
+	router.Use(middleware.AppContext())
+
+	// Setting up api group
+	// Setup Controllers for each
+	api := router.Group("api")
+	v1 := api.Group("v1")
+	users := v1.Group("users")
+	handlers.RegisterUsersRoutes(users)
+
+	// Set up Swaggo path here
+	// Currently commened will need to enable this only in test mode
+	// router.Static("/swagger", "./swagger")
+	// docs := admin.Group("docs")
+	router.GET("/docs/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	router.NoRoute(func(c *gin.Context) {
+		c.AbortWithStatusJSON(http.StatusNotFound, types.ResponseError{
+			Code:    "ERR_PAGE_NOT_FOUND",
+			Message: "Page not found",
+		})
+	})
+
+	return router
+}
